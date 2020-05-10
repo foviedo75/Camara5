4d3d01038c2ad7b26b39fc0bdbe7749a5e99e24a
Fabio Nicolás Oviedo <fabiooviedo@hotmail.com>
2020-05-10 13:54:48 -0300

    Add project files.

---
+++ Camara5/Camara5/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using Camara5.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Camara5
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new CameraPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ Camara5/Camara5/Views/CameraPage.xaml.cs
@@ -0,0 +1,72 @@
+﻿using Camara5.Models;
+using Plugin.Media;
+using Plugin.Media.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Camara5.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CameraPage : ContentPage
+    {
+        ObservableCollection<MediaModel> Photos = new ObservableCollection<MediaModel>();
+        public CameraPage()
+        {
+            InitializeComponent();
+            
+        }
+
+        private async void photoButton_Pressed(object sender, EventArgs e)
+        {
+            var isInitialize = await CrossMedia.Current.Initialize();
+            if(!CrossMedia.Current.IsCameraAvailable || !CrossMedia.Current.IsTakePhotoSupported ||  !CrossMedia.IsSupported ||!isInitialize)
+            {
+                await DisplayAlert("Error", "La camara no encuentra disponible", "Ok");
+                return;
+            }
+
+            var newPhotoID = Guid.NewGuid();
+            using (var photo = await CrossMedia.Current.TakePhotoAsync(new StoreVideoOptions
+            {
+                Name = newPhotoID.ToString(),
+                SaveToAlbum = true,
+                DefaultCamera = CameraDevice.Front,                
+                Directory = "DemoCamara",
+                PhotoSize = PhotoSize.Custom,
+                CustomPhotoSize = 30,
+                CompressionQuality = 60
+                
+            }))
+            {
+                if (string.IsNullOrWhiteSpace(photo?.Path))
+                {
+                    return;
+                }
+
+                var newPhotoMedia = new MediaModel()
+                {
+                    MediaID = newPhotoID,
+                    Path = photo.Path,
+                    LocalDateTime = DateTime.Now
+
+                };
+
+                Photos.Add(newPhotoMedia);
+
+                photo.Dispose();
+
+
+            }
+
+            listPhotos.ItemsSource = Photos;
+
+        }
+    }
+}
